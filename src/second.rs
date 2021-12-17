0a7e4c5f6e2e2f29e9b4e07be846f35fcd0cbd15
Levente Krizsan <lewalevi@gmail.com>
2021-12-17 19:58:25 +0100

    feat(second): make List generic

--- src/second.rs
+++ src/second.rs
@@ -1,23 +1,23 @@
 use std::mem;
 
-pub struct List {
-    head: Link,
+pub struct List<T> {
+    head: Link<T>,
 }
 
 /// As `Link` is basically an `Option`, use it instead of reinventing the wheel
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-struct Node {
-    elem: i32,
-    next: Link,
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
 }
 
-impl List {
+impl<T> List<T> {
     pub fn new() -> Self {
         Self { head: None }
     }
 
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem,
             // `take` is the same as `mem::replace`, but more idiomatic, i.e it moves out the value
@@ -28,7 +28,7 @@ impl List {
         self.head = Some(new_node);
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         // use `map` to apply a function to the inner value if it is available, i.e. `Some(v)`
         self.head.take().map(|node| {
             self.head = node.next;
@@ -37,7 +37,7 @@ impl List {
     }
 }
 
-impl Drop for List {
+impl<T> Drop for List<T> {
     fn drop(&mut self) {
         let mut cur_link = mem::replace(&mut self.head, None);
 
