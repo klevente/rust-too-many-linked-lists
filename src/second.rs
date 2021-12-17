9050b35539997c7ae0a8bc5330cc48991e6049cd
Levente Krizsan <lewalevi@gmail.com>
2021-12-17 20:10:09 +0100

    feat(second): test peek_mut

--- src/second.rs
+++ src/second.rs
@@ -97,11 +97,18 @@ mod test {
         let mut list = List::new();
         assert_eq!(list.peek(), None);
         assert_eq!(list.peek_mut(), None);
+
         list.push(1);
         list.push(2);
         list.push(3);
 
         assert_eq!(list.peek(), Some(&3));
         assert_eq!(list.peek_mut(), Some(&mut 3));
+
+        // test whether the value can actually be mutated
+        list.peek_mut().map(|value| *value = 42);
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
     }
 }
